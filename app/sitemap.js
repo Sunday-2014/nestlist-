c2c347d29ad6140bc4d36321d5e9e0a74b5a6531
Temesgen Getie <mailtogetie@gmail.com>
2026-06-06 19:48:23 -0400

    Add sitemap for Google SEO

---
+++ app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.enjerapresslist.com',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'https://www.enjerapresslist.com/register',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.enjerapresslist.com/login',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.enjerapresslist.com/list',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.enjerapresslist.com/contact',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.enjerapresslist.com/browse',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.9,
+    },
+  ]
+}
